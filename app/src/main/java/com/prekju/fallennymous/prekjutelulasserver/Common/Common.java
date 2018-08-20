9befbeacb30cd06e0000a60c89469f6310d5cc76
vyan7x <vianzeid@gmail.com>
2018-08-20 11:12:08 +0700

    prekju telulas server

---
+++ app/src/main/java/com/prekju/fallennymous/prekjutelulasserver/Common/Common.java
@@ -0,0 +1,20 @@
+package com.prekju.fallennymous.prekjutelulasserver.Common;
+
+import com.prekju.fallennymous.prekjutelulasserver.Model.User;
+
+/**
+ * Created by fallennymous on 14/08/2018.
+ */
+
+
+    public class Common {
+        public static User currentUser;
+
+        public static final String UPDATE = "Update";
+        public static final String DELETE = "Delete";
+
+        // Request to upload image
+        public static final int PICK_IMAGE_REQUEST = 71;
+    }
+
+
